4be3fddb45e04c55fd1f33b45f4f99d1d4958f7b
ojugz <mcdanielsojugo@gmail.com>
2022-05-05 21:10:52 +0100

    worked on the home page, and the App.js files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
 import Home from './pages/home/Home';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
@@ -16,29 +16,31 @@ function App() {
       <Topbar />
       <div className="container">
         <Sidebar />
-        
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
-        
+      <BrowserRouter>
+           <Routes>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/users">
+                  <UserList />
+                </Route>
+                <Route path="/user/:userId">
+                  <User />
+                </Route>
+                <Route path="/newUser">
+                  <NewUser />
+                </Route>
+                <Route path="/products">
+                  <ProductList />
+                </Route>
+                <Route path="/product/:productId">
+                  <Product />
+                </Route>
+                <Route path="/newproduct">
+                  <NewProduct />
+                </Route>
+          </Routes>
+        </BrowserRouter>
       </div>
     </Router>
   );
